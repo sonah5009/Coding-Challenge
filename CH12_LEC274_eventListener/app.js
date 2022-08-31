55504105c08276d9bf9b97bc0d7edb4ce0ee148d
sonah5009 <sonah5009@gmail.com>
2022-08-31 13:07:09 +0900

    Update: to day35

---
+++ CH12_LEC274_eventListener/app.js
@@ -0,0 +1,7 @@
+let paragraphElement = document.querySelector('p');
+
+function changeParagraphText() {
+  paragraphElement.textContent = 'Clicked!';
+  console.log('Paragraph clicked!');
+}
+paragraphElement.addEventListener('click', changeParagraphText)
